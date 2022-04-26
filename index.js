be97e447a5f20098e224ca3561e38cd27c0daba2
Enchanting121 <samantha.barros95@gmail.com>
2022-04-26 11:54:29 -0400

    commit tree 787933f731183348569b6f69db46ed667dc953e1

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+const hq = 42
+
+function distanceFromHqInBlocks(someNumber) {
+    let result
+
+    result = someNumber - hq;
+
+    if (result <= 0) {
+        return -(result);
+    }
+    return (result);
+
+}
+
+function distanceFromHqInFeet(someNumber) {
+    distanceFromHqInBlocks(someNumber);
+    return distanceFromHqInBlocks(someNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let result = (destination - start) * 264;
+
+    if (result <= 0) {
+        return -(result);
+    }
+    return result;
+}
+
+function calculatesFarePrice(start, destination) {
+    let result = distanceTravelledInFeet(start, destination);
+    let price
+    let cents = .02
+
+    if (result < 400) {
+        price = 0;
+    } else if (result > 400 && result < 2000) {
+        price = (result - 400) * .02;
+    } else if (result > 2000 && result < 2500) {
+        price = 25;
+    } else if (result >= 2500) {
+        price = "cannot travel that far";
+    }
+    return price;
+
+}
